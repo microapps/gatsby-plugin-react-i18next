600ba2e7657807957b555cce761eec966e8b6592
Peter Nagy <peter.nagy@microapps.com>
2020-06-24 18:12:31 +0200

    feat: don't include default language into meta urls

--- src/Helmet.tsx
+++ src/Helmet.tsx
@@ -3,18 +3,15 @@ import {Helmet as ReactHelmet, HelmetProps} from 'react-helmet';
 import {useI18next} from './useI18next';
 
 export const Helmet: React.FC<HelmetProps> = ({children, ...props}) => {
-  const {languages, language, originalPath, siteUrl = ''} = useI18next();
+  const {languages, language, originalPath, defaultLanguage, siteUrl = ''} = useI18next();
+  const createUrlWithLang = (lang: string) =>
+    `${siteUrl}${lang === defaultLanguage ? '' : `/${lang}`}${originalPath}`;
   return (
     <ReactHelmet {...props}>
       <html lang={language} />
-      <link rel="canonical" href={`${siteUrl}/${language}${originalPath}`} />
+      <link rel="canonical" href={createUrlWithLang(language)} />
       {languages.map((lang) => (
-        <link
-          rel="alternate"
-          key={lang}
-          href={`${siteUrl}/${lang}${originalPath}`}
-          hrefLang={lang}
-        />
+        <link rel="alternate" key={lang} href={createUrlWithLang(lang)} hrefLang={lang} />
       ))}
       {children}
     </ReactHelmet>
