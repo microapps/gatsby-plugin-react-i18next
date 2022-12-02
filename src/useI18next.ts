0db424dc74fbb0f68d19583c0e00b0f0b7fcde1a
George Adams <georgeadams1995@gmail.com>
2022-12-02 14:49:28 +0000

    fixup types

--- src/useI18next.ts
+++ src/useI18next.ts
@@ -1,4 +1,4 @@
-import {Namespace, useTranslation, UseTranslationOptions} from 'react-i18next';
+import {DefaultNamespace, useTranslation, UseTranslationOptions} from 'react-i18next';
 import {useContext} from 'react';
 import {navigate as gatsbyNavigate} from 'gatsby';
 import {I18nextContext} from './i18nextContext';
@@ -8,7 +8,7 @@ import {LANGUAGE_KEY} from './types';
 declare var __BASE_PATH__: string | undefined;
 declare var __PATH_PREFIX__: string | undefined;
 
-export const useI18next = (ns?: Namespace, options?: UseTranslationOptions) => {
+export const useI18next = (ns?: DefaultNamespace, options?: UseTranslationOptions) => {
   const {i18n, t, ready} = useTranslation(ns, options);
   const context = useContext(I18nextContext);
 
